19633d37bf46503511373131b5d3b9891bbecb98
aliceDeyna <83836875+aliceDeyna@users.noreply.github.com>
2021-06-25 20:23:09 +0200

    Update zapytanie_1_MR.js

--- PD_04/zapytanie_1_MR.js
+++ PD_04/zapytanie_1_MR.js
@@ -1,24 +1,25 @@
 // 1. Średnią wagę i wzrost osób w bazie z podziałem na płeć (tzn. osobno mężczyzn, osobno kobiet)
 
 db.people.mapReduce(
-  function () {
-    emit(this.sex, { weight: this.weight, height: this.height 
-      } ); },
-
-  function (key, values) {
-    let total_weight = 0;
-    let total_height = 0;
-
-    values.forEach(element => {
-      total_weight += element.weight;
-      total_height += element.height;
+    function() 
+    { emit(this.sex, { 
+        'height': parseFloat(this.height), 
+        'weight': parseFloat(this.weight), 
+        'count': 1} ) },
+        
+        function(key, values) 
+        { return { 
+            'height': Array.sum(values.map(e => e['height'])), 
+            'weight': Array.sum(values.map(e => e['weight'])), 
+            'count': Array.sum(values.map(e => e['count'])) 
+        } },
+    
+    { finalize: function(key, value) {
+            return { 
+                'avg_height': (value.height / value.count),
+                'avg_weight': (value.weight / value.count) } },
+        
+        out: 'zapytanie_1_MR'
     } );
-
-    return {
-      avg_weight: total_weight / values.length,
-      avg_height: total_height / values.length };
-  },
-  { out: 'zapytanie_1_MR' }
-);
 
 printjson(db.zapytanie_1_MR.find().toArray());
